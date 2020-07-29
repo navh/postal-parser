6af93c0e47d3c0ccbc749b77d5959556adfa104d
rizvisaira <54250336+rizvisaira@users.noreply.github.com>
2020-07-29 15:52:51 -0400

    Update submit_pyspark_job.sh

--- gcp_postal_parser/submit_pyspark_job.sh
+++ gcp_postal_parser/submit_pyspark_job.sh
@@ -1,6 +1,8 @@
 CLUSTER_NAME=postal-parser
 REGION=europe-west1
 BUCKET_NAME=postal-parser-27
+NUM_TRAIN=`ls [TRAIN DATA PATH] | wc -l`
+NUM_TEST=`ls [TEST DATA PATH] | wc -l`
 
 
 echo "Submitting the job"
@@ -10,4 +12,6 @@ gcloud dataproc jobs submit pyspark \
         --properties=spark.jars.packages=com.johnsnowlabs.nlp:spark-nlp_2.11:2.5.1 \
      --driver-log-levels root=FATAL \
      sparkNLP.py \
-     -- ${BUCKET_NAME}
+     -- ${BUCKET_NAME} \
+     -- ${NUM_TRAIN} \
+     -- ${NUM_TEST}

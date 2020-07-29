8a6801352af72b2b569f052ce18d2108e0eeb258
Monafff <67806619+Monafff@users.noreply.github.com>
2020-07-28 19:12:19 -0700

    Update submit_pyspark_job.sh

--- gcp_postal_parser/submit_pyspark_job.sh
+++ gcp_postal_parser/submit_pyspark_job.sh
@@ -1,13 +1,13 @@
 CLUSTER_NAME=postal-parser
-REGION=us-central1
-BUCKET_NAME=postal-parser
+REGION=europe-west1
+BUCKET_NAME=postal-parser-27
 
 
 echo "Submitting the job"
 
-gcloud dataproc jobs submit pyspark \ 
-	--cluster ${CLUSTER_NAME} \ 
-	--properties=spark.jars.packages=JohnSnowLabs:spark-nlp:2.5.1 \ 
-        --driver-log-levels root=FATAL \ 
-        sparkNLP.py \ 
-	-- ${BUCKET_NAME}
+gcloud dataproc jobs submit pyspark \
+        --cluster ${CLUSTER_NAME} \
+        --properties=spark.jars.packages=com.johnsnowlabs.nlp:spark-nlp_2.11:2.5.1 \
+     --driver-log-levels root=FATAL \
+     sparkNLP.py \
+     -- ${BUCKET_NAME}
